ff41c01fee33942819cb754e220807d2b49ba809
alpeevaist2 <62651118+alpeevaist2@users.noreply.github.com>
2023-01-22 21:57:15 +0300

    Update HomeWork2.java

--- src/main/java/org/future/code/homework/HomeWork2.java
+++ src/main/java/org/future/code/homework/HomeWork2.java
@@ -1,7 +1,22 @@
 package org.future.code.homework;
+import java.util.Scanner;
 
-public class HomeWork2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class AuthenticationException {
+
+    /**
+     * Создать статический метод который принимает на вход три параметра: login, password.
+     * Login должен содержать только латинские буквы, цифры и знак подчеркивания.
+     * Длина login должна быть меньше 20 символов. Если login не соответствует этим требованиям, необходимо выбросить WrongLoginException.
+     * Password должен содержать только латинские буквы, цифры и знак подчеркивания. Длина password должна быть меньше 20 символов.
+     * Если password не соответствует этим требованиям, необходимо выбросить WrongPasswordException.
+     * Обработка исключений проводится внутри метода.
+     * Метод возвращает true, если значения верны или false в другом случае.
+     * Реализавать основную работу в классе Main
+     */
+    public static boolean isValidLogin(String login) throws WrongLoginException{
+      //Ваш код
+    }
+    public static boolean isValidPassword(String login) throws WrongPasswordException{
+      //Ваш код
     }
 }
